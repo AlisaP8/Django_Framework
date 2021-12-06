f4f0f360ca21b09038a347fbb9230115ddf510f1
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2021-12-07 02:20:37 +0400

    'authentication_and_registration'

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authapp',
     'mainapp',
 ]
 
@@ -51,6 +52,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 TEMPLATES = [
     {
