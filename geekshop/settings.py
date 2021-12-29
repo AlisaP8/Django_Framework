be6fb5106ea226bcb5f73d6c861347df04a498a3
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2021-12-29 23:41:37 +0400

    'push'

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adminapp',
     'authapp',
+    'basketapp',
     'mainapp',
 ]
 
@@ -138,3 +140,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/auth/login/'
